1d3044ce3b2e3788ec631b97a2b3b2a26e6d89af
Poveda <julianopoveda@github.com>
2021-01-04 22:49:27 -0300

    CRUD Done

--- app/server.js
+++ app/server.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var app = express();
 var sqlite3 = require("sqlite3");
-var fs = require("fs");
-var file = "juri.db"
-var exists = fs.existsSync(file);
 const bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
-  var db = new sqlite3.Database(file);
-  db.serialize(function () {
-    if (!exists) {
-      console.log("db nao existe");
-      return;
-    }
+dbFactory = {
+  getConnection: () => {
+    file = "juridico.db";
+    return new sqlite3.Database(file);
+  }
+}
 
+
+app.get("/", function (req, res) {
+  var db = dbFactory.getConnection();
+  db.serialize(function () {
     processos = [];
 
     db.each("select * from processos", function (err, row) {
@@ -30,21 +30,66 @@ app.get("/", function (req, res) {
   db.close();
 });
 
+app.get("/:numero", function (req, res) {
+  var db = dbFactory.getConnection();
+  db.serialize(function () {
+    db.get("select * from processos where numero=$numero", { $numero: req.params.numero }, function (err, row) {
+      res.send(row);
+    });
+
+  });
+
+  db.close();
+});
+
 app.post("/", function (req, res) {
   var db = new sqlite3.Database(file);
-  db.run("insert into processos(numero, uf_sigla, cidade, reu, cliente, status) values(?)", req.body);
+  db.run("insert into processos(numero, uf_sigla, cidade, reu, identidade, cliente, status) values($numero, $uf_sigla, $cidade, $reu, $identidade, $cliente, $status )", {
+    $numero: req.body.numero,
+    $uf_sigla: req.body.uf_sigla,
+    $cidade: req.body.cidade,
+    $reu: req.body.reu,
+    $identidade: req.body.identidade,
+    $cliente: req.body.cliente,
+    $status: req.body.status
+  }, (err) => {
+    if (err == null)
+      res.status(202);
+
+    console.log(err);
+    res.send(err);
+    res.status(500);
+  });
   res.send("Processo lançado");
 })
 
 app.put("/status", function (req, res) {
+  var db = dbFactory.getConnection();
   db.run("update processos set status=$status where numero=$numero", {
-    $status: req.status,
-    $numero: req.numero
-  });
+    $status: req.body.status,
+    $numero: req.body.numero
+  },
+    (err) => {
+      if (err == null)
+        res.send("Status Alterado");
+
+      console.log(err);
+      res.send(err);
+      res.status(500);
+    });
 })
 
-app.delete("/", function (req, res) {
-  db.run("delete from processos where numero=$numero", {$numero: req.numero});
+app.delete("/:numero", function (req, res) {
+  var db = dbFactory.getConnection();
+  db.run("delete from processos where numero=$numero", { $numero: req.params.numero },
+    (err) => {
+      if (err == null)
+        res.send("Processo excluido");
+
+      console.log(err);
+      res.send(err);
+      res.status(500);
+    });
 })
 
 app.listen('3000');
